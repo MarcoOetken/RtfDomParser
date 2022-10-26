e1a34cbe989d896e1b11c6d20fd7f91a48644275
Marco Oetken <marco.oetken@omspire.de>
2022-10-26 20:27:57 +0200

    Added 1st unittest
    Updated NUnit.
    AddElemTwiceCountStays1 fails which shows that Add is not implemented properly. Since it is not used, it can be re removed (next step).

---
+++ Source/RtfDomParser.Tests/StringAttributeCollectionTests.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * 
+ *   Testcode RtfDomParser
+ *   Author : Marco Oetken.
+ *   Email  : marco.oetken@omspire.de
+ * 
+ */
+
+namespace RtfDomParser.Tests
+{
+    using System;
+    using NUnit.Framework;
+    using RtfDomParser;
+
+    [TestFixture]
+    public class StringAttributeCollectionTests
+    {
+        [Test]
+        public void NewObjectIsEmpty()
+        {
+            var sut = new StringAttributeCollection();
+            Assert.That(sut.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void EntriesAreCaseSensitive()
+        {
+            var sut = new StringAttributeCollection();
+            sut["Name"] = "Value";
+            Assert.That(sut["Name"], Is.EqualTo("Value"));
+            Assert.That(sut["name"], Is.EqualTo(null));
+        }
+
+        [Test]
+        public void SetValueTwiceCountStays1()
+        {
+            var sut = new StringAttributeCollection();
+            sut["Name"] = "Value";
+            sut["Name"] = "Value";
+            Assert.That(sut["Name"], Is.EqualTo("Value"));
+            Assert.That(sut.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddElemTwiceCountStays1()
+        {
+            var sut = new StringAttributeCollection();
+            sut.Add(new StringAttribute() { Name = "name", Value = "Value" });
+            sut.Add(new StringAttribute() { Name = "name", Value = "Value" });
+            Assert.That(sut["name"], Is.EqualTo("Value"));
+            Assert.That(sut.Count, Is.EqualTo(1));
+        }
+    }
+}
+
